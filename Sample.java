d91403f353e68fdd4fcb51a948b479f8f6e313e7
tanzilur <60995422+tanzilur@users.noreply.github.com>
2020-02-17 10:18:14 +0600

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Sample
+{
+  public static void main(String args[])
+  {
+    Scanner a = new Scanner (System.in);
+    String S = a nextLine ();
+	int n = S.length ();
+	for (int i=(n-1); i>=0; i--)
+	{
+		System.out.print (S. char At(i));
+	}
+  }
+}
+
+   
+    
